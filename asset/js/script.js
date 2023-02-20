d4491ebcd3a74ee2e1b166dde75658d34e61a268
WaleedGamal25 <waleedgamal16@gmail.com>
2023-02-20 04:10:55 +0200

    wg

---
+++ asset/js/script.js
@@ -0,0 +1,159 @@
+$(document).ready(function(){
+    $('.myslider').owlCarousel({
+          items:1,
+          nav: false,
+          dots: true,
+          dotsData: true,
+          rtl: true
+      });
+  });
+
+
+
+  $(document).ready(function() {
+
+
+    //Init the carousel
+    $("#owl-demo").owlCarousel({      
+      items: 1,
+      loop: true,
+      nav:true,
+      autoplay: true,
+      autoplayTimeout:10000,
+      rtl:true,
+      onInitialized: startProgressBar,
+      onTranslate: resetProgressBar,
+      onTranslated: startProgressBar
+    });
+  
+     function startProgressBar() {
+      // apply keyframe animation
+      $(".slide-progress").css({
+        width: "100%",
+        transition: "width 10000ms"
+      });
+    }
+
+    function resetProgressBar() {
+      $(".slide-progress").css({
+        width: 0,
+        transition: "width 0s"
+      });
+    }
+    //Init progressBar where elem is $("#owl-demo")
+    function progressBar(elem){
+      $elem = elem;
+      //build progress bar elements
+      buildProgressBar();
+      //start counting
+      start();
+    }
+
+ 
+ 
+});
+
+
+
+
+function showhide() {
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("toggle");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+window.onload = function() {
+  document.getElementById("myTextInput").focus();
+};
+
+
+const form = document.querySelector('.verfication-form')
+const inputs = form.querySelectorAll('.verfication-input')
+const KEYBOARDS = {
+  backspace: 8,
+  arrowLeft: 37,
+  arrowRight: 39,
+}
+
+function handleInput(e) {
+  const input = e.target
+  const nextInput = input.nextElementSibling
+  if (nextInput && input.value) {
+    nextInput.focus()
+    if (nextInput.value) {
+      nextInput.select()
+    }
+  }
+}
+
+function handlePaste(e) {
+  e.preventDefault()
+  const paste = e.clipboardData.getData('text')
+  inputs.forEach((input, i) => {
+    input.value = paste[i] || ''
+  })
+}
+
+function handleBackspace(e) { 
+  const input = e.target
+  if (input.value) {
+    input.value = ''
+    return
+  }
+  
+  input.previousElementSibling.focus()
+}
+
+function handleArrowLeft(e) {
+  const previousInput = e.target.previousElementSibling
+  if (!previousInput) return
+  previousInput.focus()
+}
+
+function handleArrowRight(e) {
+  const nextInput = e.target.nextElementSibling
+  if (!nextInput) return
+  nextInput.focus()
+}
+
+form.addEventListener('input', handleInput)
+inputs[0].addEventListener('paste', handlePaste)
+
+inputs.forEach(input => {
+  input.addEventListener('focus', e => {
+    setTimeout(() => {
+      e.target.select()
+    }, 0)
+  })
+  
+  input.addEventListener('keydown', e => {
+    switch(e.keyCode) {
+      case KEYBOARDS.backspace:
+        handleBackspace(e)
+        break
+      case KEYBOARDS.arrowLeft:
+        handleArrowLeft(e)
+        break
+      case KEYBOARDS.arrowRight:
+        handleArrowRight(e)
+        break
+      default:  
+    }
+  })
+})
+
+
+
+
+
